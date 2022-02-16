8f061f8760dd899a42e8cd3cd373c0a33e5c0368
Chloe Declan Taylor <26508550+transdisaster@users.noreply.github.com>
2022-02-16 21:54:07 +0100

    Create DynaBackground.plugin.js

---
+++ DynaBackground.plugin.js
@@ -0,0 +1,83 @@
+/**
+ * @name DynaBackground
+ * @version 1.0.0
+ * @description Simple plugin that lets you set custom backgrounds per server.
+ * @author Ash Taylor
+ *  
+*/
+
+module.exports = class DynaBackground {
+	constructor() {
+		this.backgrounds = {
+			"default": "",
+		};
+		this.lastBackground = this.backgrounds["default"];
+	}
+
+	load() {
+		
+	}
+
+	start() {
+		if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+
+		this.backgrounds = ZLibrary.Utilities.loadData("DynaBackground", "backgrounds", {"default": ""});
+
+		ZLibrary.DiscordModules.SelectedGuildStore._changeCallbacks.add(this.updateBackgrounds.bind(this));
+		this.updateBackgrounds();
+	}
+
+	stop() {
+		ZLibrary.DiscordModules.SelectedGuildStore._changeCallbacks.delete(this.updateBackgrounds.bind(this));
+	}
+
+	updateBackgrounds() {
+		let currentGuildId = ZLibrary.DiscordModules.SelectedGuildStore.getGuildId();
+		let selectedBackground = this.backgrounds["default"];
+
+		console.log("Switched to guild " + currentGuildId)
+
+		if (currentGuildId in this.backgrounds)
+			selectedBackground = this.backgrounds[currentGuildId];	
+		
+		if (selectedBackground != this.lastBackground) {
+			ZLibrary.DOMTools.removeStyle(69);
+			ZLibrary.DOMTools.addStyle(69, ":root { --background-image: url('" + selectedBackground + "') !important; --background: var(--background-image); }");
+			
+			console.log("setting background to " + selectedBackground);
+			
+			this.lastBackground = selectedBackground;
+		}
+	}
+
+	createBgCallback(key) {
+		return function(text) { 
+			if (text)
+				this.backgrounds[key] = text; 
+			else
+				delete this.backgrounds[key];
+
+			console.log("Setting " + key + " to " + text);
+
+			ZLibrary.Utilities.saveData("DynaBackground", "backgrounds", this.backgrounds);
+			this.updateBackgrounds();
+		}
+	}
+
+	getSettingsPanel() {
+		let guilds = ZLibrary.DiscordModules.GuildStore.getGuilds();
+		let backgroundsPanel = new ZLibrary.Settings.SettingGroup("Server Backgrounds", {shown: true});
+		let settingsPanel = new ZLibrary.Settings.SettingPanel();
+
+		let defaultBgCallback = this.createBgCallback("default");
+		settingsPanel.append(new ZLibrary.Settings.Textbox("Default Background", "", this.backgrounds["default"], defaultBgCallback.bind(this)));
+		settingsPanel.append(backgroundsPanel);
+		
+		for (const [guild_id, data] of Object.entries(guilds)) {
+			let callback = this.createBgCallback(guild_id);
+			backgroundsPanel.append(new ZLibrary.Settings.Textbox(data.name, "", this.backgrounds[guild_id], callback.bind(this)));
+		}
+
+		return settingsPanel.getElement();
+	}
+}
